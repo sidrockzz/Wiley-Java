ecf771ed00f8283ada2d803cb32e76c8ae441622
sidrockzz <marshtomp.1999@hotmail.com>
2021-03-23 10:51:18 +0530

    Enumeration is added

--- src/vector.java
+++ src/vector.java
@@ -1,3 +1,4 @@
+import java.util.Enumeration;
 import java.util.Vector;
 
 public class vector {
@@ -9,6 +10,23 @@ public class vector {
         v.add(new Integer(101));
         v.add("B");
         v.add(1);
+        v.add(3,"C");
         System.out.println(v);
+        v.get(3);
+        String s1 = (String) v.get(3);
+        System.out.println(s1);
+        v.set(2,"Z");
+        System.out.println(v);
+        v.remove("A");
+        System.out.println(v);
+        v.remove(3);
+        System.out.println(v);
+        //Enumeration
+        System.out.println("-----------Enumeration---------------");
+        Enumeration en = v.elements();
+        while(en.hasMoreElements()){
+            Object e = en.nextElement();
+            System.out.println(e+ " ");
+        }
     }
 }
