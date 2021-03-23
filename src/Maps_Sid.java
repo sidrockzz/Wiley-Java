733206083b6d098171b251c6cee2dac37350c622
sidrockzz <marshtomp.1999@hotmail.com>
2021-03-23 12:36:55 +0530

    TreeMap is added

--- src/Maps_Sid.java
+++ src/Maps_Sid.java
@@ -9,5 +9,17 @@ public class Maps_Sid {
         map.put("Luffy",new Double(14.11));
         map.put("Zoro",new Double(15.11));
         System.out.println(map);
+        map.replace("Sid",new Double(99.99));
+        System.out.println(map);
+        map.put("Sid",new Double(22.3));
+        System.out.println(map);
+        map.put("Sanji","Sanji");
+        map.put("Hey",null);
+        //Cant add null key
+        //Differet types of keys cant be added
+        map.put("Rayleigh",new Integer(20));
+        System.out.println(map);
+        Object e = map.get("Sid");
+        System.out.println(e);
     }
 }
