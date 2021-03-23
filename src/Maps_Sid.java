167c918a8f572ad05fd79de36eabb4783b9fc8d6
sidrockzz <marshtomp.1999@hotmail.com>
2021-03-23 12:19:00 +0530

    TreeMap is added

---
+++ src/Maps_Sid.java
@@ -0,0 +1,13 @@
+import java.util.TreeMap;
+
+public class Maps_Sid {
+    public static void main(String [] args){
+        TreeMap map = new TreeMap();
+        map.put("Sid",new Double(11.1));
+        map.put("Akash",new Double(12.1));
+        map.put("Avinash",new Double(13.11));
+        map.put("Luffy",new Double(14.11));
+        map.put("Zoro",new Double(15.11));
+        System.out.println(map);
+    }
+}
