e4e396aaba44d888698e750eeae9b885b757adce
sidrockzz <marshtomp.1999@hotmail.com>
2021-03-31 09:39:02 +0530

    Infix to Prefix

--- src/jdbc_Sid.java
+++ src/jdbc_Sid.java
@@ -3,7 +3,8 @@ public class jdbc_Sid {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
        // Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Company_DB","root","NikiSiddu3014!");
-        Statement st = con.createStatement(); ResultSet rs = st.executeQuery("Select * from cdb");
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("Select * from cdb");
         while(rs.next()){
             int id = rs.getInt(1);
             String name = rs.getString(2);
@@ -11,3 +12,4 @@ public class jdbc_Sid {
         }
     }
 }
+// path :/usr/share/java/mysql-connector-java-8.0.23.jar set it as the class module
